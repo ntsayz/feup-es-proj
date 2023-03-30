9e574dc05928d1b507fea68931e39f2bf2ca169e
Ntsay <ntsayyzacarias@gmail.com>
2023-03-30 17:19:57 +0100

    a few minor changes

--- app/lib/screens/my_teams.dart
+++ app/lib/screens/my_teams.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trabalho/backend/Groups.dart';
-
+import 'package:trabalho/screens/components/widgets.dart';
 class MyTeams extends StatefulWidget {
   final String uid;
   final Map<String, dynamic>? userData;
@@ -17,44 +17,39 @@ class _MyTeamsState extends State<MyTeams> {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: Text('What is the group name ?'),
-              content: TextField(
-                controller: _groupnamecontroller,
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(hintText: 'ex:.. Grupo de Futebol'),
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: Text('CANCEL'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                TextButton(
-                  child: Text('OK'),
-                  onPressed: () {
-                    createGroup(_groupnamecontroller.text, widget.uid); //TODO
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            );
-          },
+    return YellowButton(text: "CREATE GROUP", height: 50, width: double.infinity, onItemTapped:() => getGroupName());
+  }
+
+  void getGroupName(){
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('What is the group name ?'),
+          content: TextField(
+            controller: _groupnamecontroller,
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(hintText: 'ex:.. Grupo de Futebol'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                createGroup(_groupnamecontroller.text, widget.uid); //TODO
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
-      child: Text('Press me'),);
+    );
   }
 
-
-
-  void hello(){
-
-  }
 }
 
