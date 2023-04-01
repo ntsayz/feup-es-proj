ff356505763884d369272ff09aae59d4e5e8ddc2
gnlpc <up202103336@fe.up.pt>
2023-04-01 16:17:50 +0100

    groups page created

--- app/lib/backend/Groups.dart
+++ app/lib/backend/Groups.dart
@@ -28,11 +28,9 @@ Future<void> addUIDToGroup (String ID, String groupID) async{
   await _firestore.collection('user').doc(ID).update({'groups': FieldValue.arrayUnion([groupID])});
 }
 
-
 Future<void> addUserToGroup (User? user, String groupID) async{
   await _firestore.collection('groups').doc(groupID).update({'users': FieldValue.arrayUnion([user?.uid])});
 }
-
 
 Future<List<String?>> getAllGroups() async {
   final QuerySnapshot<Map<String, dynamic>> querySnapshot =
@@ -44,7 +42,6 @@ Future<List<String?>> getAllGroups() async {
   return fieldValues;
 }
 
-
 Future<List<String>> getAllGroupsID() async {
   final QuerySnapshot<Map<String, dynamic>> querySnapshot =
   await FirebaseFirestore.instance.collection('groups').get();
@@ -52,7 +49,6 @@ Future<List<String>> getAllGroupsID() async {
   final List<String> documentIds = documents.map((doc) => doc.id).toList();
   return documentIds;
 }
-
 
 Future<String?> getGroupID(String fieldValue) async {
   final QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
@@ -67,4 +63,26 @@ Future<String?> getGroupID(String fieldValue) async {
   } else {
   return null;
   }
+}
+
+Future<String?> getGroupName (String ID) async{
+  // Get a reference to the Firestore collection
+  CollectionReference collectionReference = FirebaseFirestore.instance.collection('groups');
+
+  // Get the document with the given ID
+  DocumentSnapshot documentSnapshot = await collectionReference.doc(ID).get();
+
+  // If the document exists, return the value of the "myValue" field
+  if (documentSnapshot.exists) {
+    return documentSnapshot.get('Group Name');
+  } else {
+    throw Exception('Document not found');
+  }
+}
+
+Future<List<String>> getUIDGroups(String UID) async {
+  final DocumentSnapshot<Map<String, dynamic>> documentSnapshot = await FirebaseFirestore.instance.collection('user').doc(UID).get();
+  final List<dynamic> arrayField = documentSnapshot.data()!['groups'];
+  final List<String> stringArray = List<String>.from(arrayField.map((element) => element.toString()));
+  return stringArray;
 }
